b7dfe02affe3874b7885eedcf1ed3f00073bcd7d
demiranda <demiranda95@hotmail.com>
2023-02-23 21:33:47 -0300

    Entrega Final

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-const formatter = new Intl.NumberFormat("es-CL", { style: "currency", currency: "CLP" });
+if (location.pathname === "/") {
+	location.replace("/index.html");
+}
+
+const formatter = new Intl.NumberFormat("es-CL", {style: "currency",currency: "CLP",});
 
 //Captura de Nodos DOM
 let displayProductos = document.getElementById("displayProductos");
@@ -16,23 +20,23 @@ let carritoDetalle = document.getElementById("carritoDetalle");
 // Clase Constructora para productos
 class Producto {
 	constructor(id, nombre, categoria, genero, precio, img) {
-		this.id = id,
-		this.nombre = nombre,
-		this.categoria = categoria,
-		this.genero = genero,
-		this.precio = precio,
-		this.img = img,
-		this.cantidad = 1
+		(this.id = id),
+		(this.nombre = nombre),
+		(this.categoria = categoria),
+		(this.genero = genero),
+		(this.precio = precio),
+		(this.img = img),
+		(this.cantidad = 1);
 	}
 
 	//Metodos
 	sumarUni() {
 		this.cantidad = this.cantidad + 1;
-		return this.cantidad
+		return this.cantidad;
 	}
 	restarUni() {
 		this.cantidad = this.cantidad - 1;
-		return this.cantidad
+		return this.cantidad;
 	}
 }
 
@@ -44,10 +48,10 @@ const cargarCatalogo = async () => {
 		title: "Cargando catálogo...",
 		allowOutsideClick: false,
 		didOpen: async () => {
-			Swal.showLoading()
+			Swal.showLoading();
 			try {
-				const response = await fetch("../api/productos.json")
-				const data = await response.json()
+				const response = await fetch("../api/productos.json");
+				const data = await response.json();
 				for (const producto of data) {
 					const productoNuevo = new Producto(
 						producto.id,
@@ -57,7 +61,7 @@ const cargarCatalogo = async () => {
 						producto.precio,
 						producto.img
 					);
-					catalogo.push(productoNuevo)
+					catalogo.push(productoNuevo);
 				}
 				localStorage.setItem("catalogo", JSON.stringify(catalogo));
 			} catch (error) {
@@ -65,16 +69,14 @@ const cargarCatalogo = async () => {
 					icon: "error",
 					title: "Error cargando catálogo",
 					text: error.message,
-				})
+				});
 			}
-			Swal.hideLoading()
-			loadingCatalogo.close()
+			Swal.hideLoading();
+			loadingCatalogo.close();
 		},
 		showConfirmButton: false,
 	});
 };
-
-
 
 function inicializarCatalogo() {
 	if (localStorage.getItem("catalogo")) {
@@ -86,46 +88,43 @@ function inicializarCatalogo() {
 				producto.genero,
 				producto.precio,
 				producto.img
-			)
-			catalogo.push(productoNuevo)
+			);
+			catalogo.push(productoNuevo);
 		}
-		console.log(catalogo)
+		console.log(catalogo);
 	} else {
-		console.log("Estableciendo Stock de Vestuario")
-		cargarCatalogo()
+		console.log("Estableciendo Stock de Vestuario");
+		cargarCatalogo();
 	}
 }
-
-
-
 
 //INDEX
 
 // Function para obtener 4 productos aleatorios del catálogo
 async function obtenerProductosAleatorios() {
-	const productosAleatorios = []
+	const productosAleatorios = [];
 
-	const response = await fetch("api/productos.json")
-	const data = await response.json()
-	
+	const response = await fetch("api/productos.json");
+	const data = await response.json();
+
 	for (let i = 0; i < 3; i++) {
-	const indiceAleatorio = Math.floor(Math.random() * data.length)
-	const productoAleatorio = data[indiceAleatorio]
-	
-	const productoNuevo = new Producto(
-		productoAleatorio.id,
-		productoAleatorio.nombre,
-		productoAleatorio.categoria,
-		productoAleatorio.genero,
-		productoAleatorio.precio,
-		productoAleatorio.img
-	);
-	productosAleatorios.push(productoNuevo)
-	
-	data.splice(indiceAleatorio, 1)
+		const indiceAleatorio = Math.floor(Math.random() * data.length);
+		const producto = data[indiceAleatorio];
+
+		const productoNuevo = new Producto(
+			producto.id,
+			producto.nombre,
+			producto.categoria,
+			producto.genero,
+			producto.precio,
+			producto.img
+		);
+		productosAleatorios.push(productoNuevo);
+
+		data.splice(indiceAleatorio, 1);
 	}
-	
-	return productosAleatorios
+
+	return productosAleatorios;
 }
 
 // Function para mostrar productos aleatorios en el HTML
@@ -137,19 +136,21 @@ async function mostrarProductosAleatorios() {
 	let htmlProductosAleatorios = "";
 
 	for (const producto of productosAleatorios) {
-	const nuevoProducto = document.createElement("div");
-	nuevoProducto.classList.add(
-		"col-12",
-		"col-md-6",
-		"col-lg-3",
-		"my-3",
-		"d-flex",
-		"justify-content-center"
-	);
+		const nuevoProducto = document.createElement("div");
+		nuevoProducto.classList.add(
+			"col-12",
+			"col-md-6",
+			"col-lg-3",
+			"my-3",
+			"d-flex",
+			"justify-content-center"
+		);
 
-	nuevoProducto.innerHTML = `
+		nuevoProducto.innerHTML = `
 		<div id="${producto.id}" class="card" style="width: 18rem;">
-		<img class="card-img-top img-fluid" src="${producto.img}" alt="${producto.nombre}">
+		<img class="card-img-top img-fluid" src="${producto.img}" alt="${
+			producto.nombre
+		}">
 		<div class="card-body d-flex flex-column justify-content-center align-items-center">
 			<h6 class="card-title">${producto.nombre}</h6>
 			<p>${producto.categoria} | ${producto.genero}</p>
@@ -158,56 +159,54 @@ async function mostrarProductosAleatorios() {
 		</div>
 		</div>
 	`;
-	htmlProductosAleatorios += nuevoProducto.outerHTML;
+		htmlProductosAleatorios += nuevoProducto.outerHTML;
 	}
 
 	contenedorProductos.innerHTML = htmlProductosAleatorios;
 }
 
-
-
 //PRODUCTOS
 
 //Mostrar categorias sidebar
 function obtenerCategorias(array) {
-	return array.map((item) => item.category)
+	return array.map((item) => item.category);
 }
 
 function mostrarCategorias(array) {
 	let contador = array.reduce((contador, item) => {
 		if (!contador[item.categoria]) {
-			contador[item.categoria] = 0
+			contador[item.categoria] = 0;
 		}
-		contador[item.categoria]++
+		contador[item.categoria]++;
 		return contador;
 	}, {});
 
 	for (let item in contador) {
-		let nuevaCategoria = document.createElement("button")
+		let nuevaCategoria = document.createElement("button");
 		nuevaCategoria.classList.add(
 			"list-group-item",
 			"d-flex",
 			"justify-content-between",
 			"align-items-center"
 		);
-		nuevaCategoria.setAttribute(`id`, `filtrar${item}`)
+		nuevaCategoria.setAttribute(`id`, `filtrar${item}`);
 		nuevaCategoria.innerHTML = `
                 ${item}
-            <span class="badge bg-primary rounded-pill">${contador[item]}</span>`
+            <span class="badge bg-primary rounded-pill">${contador[item]}</span>`;
 		displayCategorias.appendChild(nuevaCategoria);
 
-		let filtrar = document.getElementById(`filtrar${item}`)
+		let filtrar = document.getElementById(`filtrar${item}`);
 		filtrar.addEventListener("click", () => {
-			buscarProductos(item, catalogo)
+			buscarProductos(item, catalogo);
 		});
 	}
 }
 
 //Function para visualizar los productos
 function mostrarCatalogo(array) {
-	displayProductos.innerHTML = ""
+	displayProductos.innerHTML = "";
 	for (let item of array) {
-		let nuevoProducto = document.createElement("div")
+		let nuevoProducto = document.createElement("div");
 		nuevoProducto.classList.add(
 			"col-12",
 			"col-md-6",
@@ -218,20 +217,24 @@ function mostrarCatalogo(array) {
 		);
 		nuevoProducto.innerHTML = `
         <div id="${item.id}" class="card" style="width: 18rem;">
-                <img class="card-img-top img-fluid" src="../${item.img}" alt="${item.nombre}">
+                <img class="card-img-top img-fluid" src="../${item.img}" alt="${
+			item.nombre
+		}">
 				<div class="card-body d-flex flex-column justify-content-center align-items-center">
 					<h4 class="card-title">${item.nombre}</h4>
 					<p>${item.genero}</p>
 					<p>${item.categoria}</p>
 					<p class="fw-bold">${formatter.format(item.precio)}</p>
-					<button id="agregarBtn${item.id}" class="btn btn-secondary bg-gris-oscuro text-resalto border-0">Agregar al Carrito <i class="bi bi-cart"></i></button>
+					<button id="agregarBtn${
+						item.id
+					}" class="btn btn-secondary bg-gris-oscuro text-resalto border-0">Agregar al Carrito <i class="bi bi-cart"></i></button>
                 </div>
         </div>`;
-		displayProductos.appendChild(nuevoProducto)
+		displayProductos.appendChild(nuevoProducto);
 
-		let agregarBtn = document.getElementById(`agregarBtn${item.id}`)
+		let agregarBtn = document.getElementById(`agregarBtn${item.id}`);
 		agregarBtn.addEventListener("click", () => {
-			agregarProductoCarrito(item)
+			agregarProductoCarrito(item);
 		});
 	}
 }
@@ -240,7 +243,7 @@ function mostrarCatalogo(array) {
 let carritoCompras = [];
 if (localStorage.getItem("carrito")) {
 	for (let producto of JSON.parse(localStorage.getItem("carrito"))) {
-		let cantidadStorage = producto.cantidad
+		let cantidadStorage = producto.cantidad;
 		let productoCarrito = new Producto(
 			producto.id,
 			producto.nombre,
@@ -249,20 +252,20 @@ if (localStorage.getItem("carrito")) {
 			producto.precio,
 			producto.img
 		);
-		productoCarrito.cantidad = cantidadStorage
-		carritoCompras.push(productoCarrito)
+		productoCarrito.cantidad = cantidadStorage;
+		carritoCompras.push(productoCarrito);
 	}
 } else {
-	carritoCompras = []
+	carritoCompras = [];
 }
 
 //Agregar productos al Carrito
 function agregarProductoCarrito(producto) {
-	let productoEnCarrito = carritoCompras.find((i) => i.id === producto.id)
+	let productoEnCarrito = carritoCompras.find((i) => i.id === producto.id);
 	if (productoEnCarrito == undefined) {
-		producto.cantidad = 1
-		carritoCompras.push(producto)
-		localStorage.setItem("carrito", JSON.stringify(carritoCompras))
+		producto.cantidad = 1;
+		carritoCompras.push(producto);
+		localStorage.setItem("carrito", JSON.stringify(carritoCompras));
 		//Sweet Alert
 		Swal.fire({
 			icon: "success",
@@ -270,33 +273,40 @@ function agregarProductoCarrito(producto) {
 			text: `Producto ${producto.nombre} ha sido agregado al carrito`,
 			showConfirmButton: false,
 			timer: 1500,
-		})
+		});
 	} else {
 		productoEnCarrito.cantidad += 1;
-		localStorage.setItem("carrito", JSON.stringify(carritoCompras))
+		localStorage.setItem("carrito", JSON.stringify(carritoCompras));
 		//Toastify
 		Toastify({
 			text: `${producto.nombre} se ha agregado una unidad`,
 			duration: 1500,
 			backgroundColor: "#198754",
-		}).showToast()
+		}).showToast();
 	}
 }
 
 function actualizarLocalStorage(array) {
-	localStorage.setItem("carrito", JSON.stringify(array))
+	localStorage.setItem("carrito", JSON.stringify(array));
 }
 
 //Mostrar productos del Carrito
 function mostrarCarrito() {
 	// Actualizar el carrito desde el localStorage
 	if (localStorage.getItem("carrito")) {
-		carritoCompras = []
+		carritoCompras = [];
 		for (let producto of JSON.parse(localStorage.getItem("carrito"))) {
-			let cantidadStorage = producto.cantidad
-			let productoCarrito = new Producto(producto.id, producto.nombre, producto.categoria, producto.genero, producto.precio, producto.img)
-			productoCarrito.cantidad = cantidadStorage
-			carritoCompras.push(productoCarrito)
+			let cantidadStorage = producto.cantidad;
+			let productoCarrito = new Producto(
+				producto.id,
+				producto.nombre,
+				producto.categoria,
+				producto.genero,
+				producto.precio,
+				producto.img
+			);
+			productoCarrito.cantidad = cantidadStorage;
+			carritoCompras.push(productoCarrito);
 		}
 	}
 
@@ -310,10 +320,18 @@ function mostrarCarrito() {
                 <td>${item.nombre}</td>
                 <td>${item.cantidad}</td>
                 <td>${formatter.format(item.precio)}</td>
-                <td class="fw-bold">${formatter.format(item.cantidad * item.precio)}</td>
-                <td><button class= "btn btn-success" id="sumarUnidadBtn${item.id}"><i class=""></i>+</button>
-                <button class= "btn btn-danger" id="restarUnidadBtn${item.id}"><i class=""></i>-</button>
-                <button class="btn btn-outline-danger" id="eliminarBtn${item.id}">Eliminar</button></td>
+                <td class="fw-bold">${formatter.format(
+									item.cantidad * item.precio
+								)}</td>
+                <td><button class= "btn btn-success" id="sumarUnidadBtn${
+									item.id
+								}"><i class=""></i>+</button>
+                <button class= "btn btn-danger" id="restarUnidadBtn${
+									item.id
+								}"><i class=""></i>-</button>
+                <button class="btn btn-outline-danger" id="eliminarBtn${
+									item.id
+								}">Eliminar</button></td>
             </tr>
             `;
 	}
@@ -346,51 +364,59 @@ function mostrarCarrito() {
 	// Agregar evento click a los botones eliminar
 	for (let item of carritoCompras) {
 		// Boton Eliminar Total Carrito
-		document.getElementById(`eliminarBtn${item.id}`).addEventListener("click", () => {
-			let productoRow = document.getElementById(`productoCarrito${item.id}`)
-			productoRow.remove()
+		document
+			.getElementById(`eliminarBtn${item.id}`)
+			.addEventListener("click", () => {
+				let productoRow = document.getElementById(`productoCarrito${item.id}`);
+				productoRow.remove();
 
-			let eliminarProducto = carritoCompras.filter((i) => i.id == item.id)
-			let eliminarPosicion = carritoCompras.indexOf(eliminarProducto[0])
-			carritoCompras.splice(eliminarPosicion, 1)
+				let eliminarProducto = carritoCompras.filter((i) => i.id == item.id);
+				let eliminarPosicion = carritoCompras.indexOf(eliminarProducto[0]);
+				carritoCompras.splice(eliminarPosicion, 1);
 
-			actualizarLocalStorage(carritoCompras)
-			mostrarCarrito()
-			calcularTotal(carritoCompras)
+				actualizarLocalStorage(carritoCompras);
+				mostrarCarrito();
+				calcularTotal(carritoCompras);
 
-			// Toastify
-			Toastify({
-				text: `${item.nombre} ha sido eliminado`,
-				duration: 1500,
-				backgroundColor: "#dc3545",
-			}).showToast();
-		});
+				// Toastify
+				Toastify({
+					text: `${item.nombre} ha sido eliminado`,
+					duration: 1500,
+					backgroundColor: "#dc3545",
+				}).showToast();
+			});
 
 		//Sumar Unidad de Producto Carrito
-		document.getElementById(`sumarUnidadBtn${item.id}`).addEventListener("click", () => {
-			console.log(`funciona sumar unidad`)
-			item.sumarUni()
-			actualizarLocalStorage(carritoCompras)
-			mostrarCarrito(carritoCompras)
-		});
+		document
+			.getElementById(`sumarUnidadBtn${item.id}`)
+			.addEventListener("click", () => {
+				console.log(`funciona sumar unidad`);
+				item.sumarUni();
+				actualizarLocalStorage(carritoCompras);
+				mostrarCarrito(carritoCompras);
+			});
 
 		//Restar Unidad de Producto Carrito
-		document.getElementById(`restarUnidadBtn${item.id}`).addEventListener("click", () => {
-			let eliminarUnidad = item.restarUni()
-			if (eliminarUnidad < 1) {
-				let productoRow = document.getElementById(`productoCarrito${item.id}`)
-				productoRow.remove()
-				let eliminarProducto = array.findIndex((i) => i.id == item.id)
-				array.splice(eliminarProducto, 1)
-				actualizarLocalStorage()
-				calcularTotal(carritoCompras)
-			} else {
-				actualizarLocalStorage(carritoCompras)
-			}
-			mostrarCarrito(carritoCompras)
-		});
+		document
+			.getElementById(`restarUnidadBtn${item.id}`)
+			.addEventListener("click", () => {
+				let eliminarUnidad = item.restarUni();
+				if (eliminarUnidad < 1) {
+					let productoRow = document.getElementById(
+						`productoCarrito${item.id}`
+					);
+					productoRow.remove();
+					let eliminarProducto = array.findIndex((i) => i.id == item.id);
+					array.splice(eliminarProducto, 1);
+					actualizarLocalStorage();
+					calcularTotal(carritoCompras);
+				} else {
+					actualizarLocalStorage(carritoCompras);
+				}
+				mostrarCarrito(carritoCompras);
+			});
 	}
-	calcularTotal(carritoCompras)
+	calcularTotal(carritoCompras);
 }
 
 function calcularTotal(array) {
@@ -398,14 +424,24 @@ function calcularTotal(array) {
 		(acc, item) => acc + item.precio * item.cantidad,
 		0
 	);
-	montoTotal == 0 ? (document.getElementById("montoTotal").innerHTML = `No hay productos en el carrito`) : (document.getElementById("montoTotal").innerHTML = `TOTAL: ${formatter.format(montoTotal)}`);
+	montoTotal == 0
+		? (document.getElementById(
+				"montoTotal"
+		  ).innerHTML = `No hay productos en el carrito`)
+		: (document.getElementById(
+				"montoTotal"
+		  ).innerHTML = `TOTAL: ${formatter.format(montoTotal)}`);
 
 	return montoTotal;
 }
 
 // Buscador de productos
 function buscarProductos(buscado, array) {
-	let busquedaArray = array.filter((item) => item.nombre.toLowerCase().includes(buscado.toLowerCase()) || item.categoria.toLowerCase().includes(buscado.toLowerCase()) || item.genero.toLowerCase().includes(buscado.toLowerCase())
+	let busquedaArray = array.filter(
+		(item) =>
+			item.nombre.toLowerCase().includes(buscado.toLowerCase()) ||
+			item.categoria.toLowerCase().includes(buscado.toLowerCase()) ||
+			item.genero.toLowerCase().includes(buscado.toLowerCase())
 	);
 	//condicional sino encuentra nada:
 	if (busquedaArray.length == 0) {
@@ -529,7 +565,9 @@ async function administrarProductos() {
                 <th><img src="../${item.img}" class="thumbnail"></th>
                 <td>${item.nombre}</td>
                 <td>${formatter.format(item.precio)}</td>
-                <td><button class="btn btn-outline-danger" id="eliminar${item.id}"><i class="bi bi-trash3"></i></button></td>
+                <td><button class="btn btn-outline-danger" id="eliminar${
+									item.id
+								}"><i class="bi bi-trash3"></i></button></td>
             </tr>
             `;
 		}
@@ -608,7 +646,9 @@ function imprimirProductosCheckout() {
             <td>${item.nombre}</td>
             <td>${item.cantidad}</td>
             <td>${formatter.format(item.precio)}</td>
-            <td class="fw-bold">${formatter.format(item.cantidad * item.precio)}</td>
+            <td class="fw-bold">${formatter.format(
+							item.cantidad * item.precio
+						)}</td>
         </tr>
 		</tbody>
         `;
@@ -656,7 +696,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
 	if (document.URL.includes("productos.html")) {
-		inicializarCatalogo()
+		inicializarCatalogo();
 		mostrarCatalogo(catalogo);
 		mostrarCategorias(catalogo);
 
@@ -680,12 +720,12 @@ document.addEventListener("DOMContentLoaded", () => {
 		const borrarFiltroBtn = document.getElementById("borrarFiltroBtn");
 
 		borrarFiltroBtn.addEventListener("click", () => {
-			inicializarCatalogo()
+			inicializarCatalogo();
 			mostrarCatalogo(catalogo);
 		});
 
 		botonCarrito.addEventListener("click", function () {
-			inicializarCatalogo()
+			inicializarCatalogo();
 			mostrarCarrito();
 		});
 	}
@@ -693,21 +733,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
 	if (document.URL.includes("login.html")) {
-		inicializarCatalogo()
+		inicializarCatalogo();
 		// Vincula el evento "submit" del formulario con la función validarUsuario
-		document.getElementById("login-form").addEventListener("submit", validarUsuario);
+		document
+			.getElementById("login-form")
+			.addEventListener("submit", validarUsuario);
 	}
 });
 
 document.addEventListener("DOMContentLoaded", () => {
 	if (document.URL.includes("admin.html")) {
 		guardarProductoBtn.addEventListener("click", () => {
-			inicializarCatalogo()
+			inicializarCatalogo();
 			agregarProducto(catalogo);
 		});
 		administrarProductos(catalogo);
 		botonCarrito.addEventListener("click", function () {
-			inicializarCatalogo()
+			inicializarCatalogo();
 			mostrarCarrito();
 		});
 	}
@@ -715,7 +757,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
 	if (document.URL.includes("checkout.html")) {
-		inicializarCatalogo()
+		inicializarCatalogo();
 		imprimirProductosCheckout();
 	}
 });
